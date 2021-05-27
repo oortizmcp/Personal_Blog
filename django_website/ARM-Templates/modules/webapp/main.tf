3698e28eebe0408ac37e95cddf744ed560c49da4
Omar Ortiz <oortiz.mcp@outlook.com>
2021-05-27 17:23:22 -0500

    added reserved parameter since kind is linux on webapp module

--- django_website/ARM-Templates/modules/webapp/main.tf
+++ django_website/ARM-Templates/modules/webapp/main.tf
@@ -8,6 +8,7 @@ resource "azurerm_app_service_plan" "asp" {
   location            = var.location
   resource_group_name = var.resource_group_name
   kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = var.asptier
